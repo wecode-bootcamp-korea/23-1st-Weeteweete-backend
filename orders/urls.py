61c13801a2418ed1cd53fdba7a0648f41c63825d
preferbaik <baik9261@gmail.com>
2021-08-05 17:16:53 +0900

    make order page and read order page3

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,6 @@ from django.urls  import path
 from .views       import OrderView
 
 urlpatterns = [
-    path('/item/<int:item_id>', OrderView.as_view()),
+    path('/<int:item_id>', OrderView.as_view()),
     path('', OrderView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -27,42 +27,26 @@ class OrderView(View):
 
     @login
     def get(self, request):
-        result      = []
         order_id    = Order.objects.get(member_id=request.user.id, status_id =1).id
         order_items = OrderItem.objects.filter(order_id=order_id) 
         
-        for order_item in order_items:
-            informations = []
-            items        = [order_item.item for order_item in order_items]
-            for item in items:
-                informations.append(
-                    {
-                    "product_name"     : item.name,
-                    "product_image"    : item.image_set.get(main=1).image_url,
-                    "product_price"    : item.price,
-                    "product_discount" : item.discount
-                    }
-                )
-            result.append(
-                {
-                "order_number" : order_item.order_id,
-                "quantity"     : order_item.quantity,
-                "information"  : informations,
-                "name"         : request.user.name,
-                "phone_number" : request.user.phone_number,
-                "address"      : request.user.address
-                }
-            )
+        result = [
+            {
+        "order_number" : order_item.order_id,
+        "quantity"     : order_item.quantity,
+        "name"         : request.user.name,
+        "phone_number" : request.user.phone_number,
+        "address"      : request.user.address,
+        "information"  : [
+            {
+            "product_name"     : item.name,
+            "product_image"    : item.image_set.get(main=1).image_url,
+            "product_price"    : item.price,
+            "product_discount" : item.discount   
+            }
+            for item in [order_item.item for order_item in order_items]
+            ]
+        }
+        for order_item in order_items]
+        
         return JsonResponse({'RESULT': result}, status=200)
-
-
-
-
-
-
-
-
-
-
-
-
