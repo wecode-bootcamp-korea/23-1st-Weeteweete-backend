e87f065ad953970015239996426d3e01ea0c41c1
임종성 <qkqndudnxld@gmail.com>
2021-08-05 23:38:32 +0900

    Account & Password View

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls  import path
-from users.views  import SignUpView, LoginView
+from users.views  import SignUpView, LoginView, FindMemberView, NewPasswordView
 
 urlpatterns = [
     path('/singup', SignUpView.as_view()),
     path('/singin', LoginView.as_view()),
+    path('/account', FindMemberView.as_view()),
+    path('/password', NewPasswordView.as_view()),
 ]
