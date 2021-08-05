7310eef282526c4c919ea6193bdaf2f2678d9d1e
임종성 <qkqndudnxld@gmail.com>
2021-08-05 16:34:57 +0900

    Products View

--- products/views.py
+++ products/views.py
@@ -6,36 +6,33 @@ from django.views    import View
 from products.models import Product, Category, Concept, Item
 
 class MenuPageView(View):
-     def get(self, request, product_id, category_id, option_id):
+     def get(self, request):
         try:
+            product_id  = request.GET.get('product_id', None)
+            category_id = request.GET.get('category_id', None)
+            option_id   = request.GET.get('option_id', None)
+
             if not Product.objects.filter(id=product_id).exists() or not Category.objects.filter(id=category_id).exists():
                 return JsonResponse({"MESSAGE":"NO_MENU"}, status=400)
             
             concepts = Concept.objects.filter(category_id = category_id)
-            results = []
-            for concept in concepts:
-                informations = []
-                items = Item.objects.filter(concept_id=concept.id, option_id=option_id)
-                for item in items:
-                    informations.append(
-                        {
-                            "id"       : item.id,
-                            "name"     : item.name,
-                            "price"    : item.price,
-                            "discount" : item.discount,
-                            "stock"    : item.stock,
-                            "color"    : item.color.name,
-                            "image"    : item.image_set.get(main=1).image_url
-                        }
-                    )
-                results.append(
-                    {
+            results = [
+                {
                         "concept"             : concept.name,
                         "concept_description" : concept.content,
                         "concpet_id"          : concept.id,
-                        "information"         : informations
-                    }
-                )
+                        "information"         : [
+                            {
+                                "id"       : item.id,
+                                "name"     : item.name,
+                                "price"    : item.price,
+                                "discount" : item.discount,
+                                "stock"    : item.stock,
+                                "color"    : item.color.name,
+                                "image"    : item.image_set.get(main=1).image_url
+                            } for item in Item.objects.filter(concept_id=concept.id, option_id=option_id)]
+                } for concept in concepts]
+
             return JsonResponse({'results':results}, status=200)
         except KeyError:
             return JsonResponse({'MESSAGE': "KEY_ERROR"}, status=400)
