7c6855efd73920e88aa7751fad7f0814e897e89f
preferbaik <baik9261@gmail.com>
2021-08-09 15:08:24 +0900

    Cart CRUD

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls  import path
 from .views       import OrderView, PurchaseView
 
 urlpatterns = [
-    path('/<int:item_id>', OrderView.as_view()),
+    path('', OrderView.as_view()),
     path('', OrderView.as_view()),
     path('/purchase', PurchaseView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,24 +1,36 @@
 import json
 
-from django.http     import JsonResponse
-from django.views    import View
-from django.db       import transaction
+from django.http      import JsonResponse
+from django.views     import View
+from django.db        import transaction
+from django.db.models import Q, F
 
-from products.models import Item
-from orders.models   import Order, OrderItem, Location
-from users.models    import Member
-from users.utils     import login
+from products.models  import Item
+from orders.models    import Order, OrderItem, Location, Cart
+from users.models     import Member
+from users.utils      import login
 
 class OrderView(View):
     @login
     @transaction.atomic
-    def post(self, request, item_id):
-        data = json.loads(request.body)
-        
+    def post(self, request):
+        item_id = request.GET.get("item_id", None)
+        data    = json.loads(request.body)      
+                
         if Order.objects.filter(member_id = request.user.id, status_id = 1).exists():
             Order.objects.filter(member_id = request.user.id, status_id = 1).delete()
 
         order = Order.objects.create(member_id = request.user.id, status_id = 1, location_id = None)
+        
+        if not item_id:
+            for cart in Cart.objects.filter(member_id = request.user.id):
+                OrderItem.objects.create(
+                    item_id              = cart.item_id,
+                    order_id             = order.id,
+                    order_item_status_id = 1,
+                    quantity             = cart.quantity
+                )
+            return JsonResponse({'MESSAGE': "SUCCESS"}, status=201)
 
         OrderItem.objects.create(
             item_id              = item_id,
@@ -26,12 +38,12 @@ class OrderView(View):
             order_item_status_id = 1,
             quantity             = data['quantity']
         )
+        
         return JsonResponse({'MESSAGE': "SUCCESS"}, status=201)
 
     @login
     def get(self, request):
-        order_id    = Order.objects.get(member_id=request.user.id, status_id =1).id
-        order_items = OrderItem.objects.filter(order_id=order_id) 
+        order_items = OrderItem.objects.filter(order_id=Order.objects.get(member_id=request.user.id, status_id =1).id) 
         
         result = [
             {
@@ -74,6 +86,7 @@ class PurchaseView(View):
             
             member.points = member.points - data['total_price']
             member.save()            
+            
 
             order             = Order.objects.get(id=data['order_id'])
             order.location_id = location.id
@@ -86,6 +99,10 @@ class PurchaseView(View):
                 item                            = Item.objects.get(id=order_item.item_id)
                 item.order_quantity             += order_item.quantity
                 item.stock                      -= order_item.quantity               
+                
+                if Cart.objects.filter(Q(member_id=request.user.id) & Q(item_id = order_item.item_id)).exists():
+                    Cart.objects.filter(Q(member_id=request.user.id) & Q(item_id = order_item.item_id)).delete()
+
                 order_item.save()
                 item.save()
 
@@ -93,3 +110,88 @@ class PurchaseView(View):
         
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+
+
+
+class CartView(View):
+    @login
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if Cart.objects.filter(Q(item_id = data['item_id']) & Q(member_id=request.user.id)).exists():
+                return JsonResponse({"MESSAGE":"CART_EXIST"}, status=400)
+            
+            if data['quantity'] > Item.objects.get(id = data['item_id']).stock:
+                return JsonResponse({"MESSAGE":"NO_STOCK"}, status=400)
+                
+            Cart.objects.create(
+                item_id=data['item_id'], 
+                member_id = request.user.id, 
+                quantity = data['quantity']
+            )
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+
+    @login
+    def get(self, request):
+
+        if not Cart.objects.filter(member_id=request.user.id).exists():
+                return JsonResponse({"MESSAGE":"EMPTY_CART"}, status=400)
+
+        results = [
+            {
+            "cart_id"  : cart.id,
+            "item_id"  : cart.item.id,
+            "quantity" : cart.quantity,
+            "name"     : cart.item.name,
+            "image"    : cart.item.image_set.get(main=1).image_url,
+            "price"    : cart.item.price,
+            "discount" : cart.item.discount
+            } 
+        for cart in Cart.objects.filter(member_id=request.user.id)]
+
+        return JsonResponse({"RESULT" : results}, status=201)
+
+    @login
+    def patch(self, request):
+        is_cart = request.GET.get("is_cart", None)
+        item_id = request.GET.get("item_id", None)
+        quantities = request.GET.get("quantity", None)
+        cart_id = request.GET.get("cart_id", None)
+
+        if not is_cart:
+            cart = Cart.objects.get(Q(item_id = item_id) & Q(member_id=request.user.id))
+            if cart.item.stock < cart.quantity + int(quantities):
+                return JsonResponse({"MESSAGE":"NO_STOCK"}, status=400)
+
+            cart.quantity = F('quantity') + int(quantities)
+            cart.save()
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+
+        cart = Cart.objects.get(id=cart_id)
+        if cart.item.stock < int(quantities):
+            return JsonResponse({"MESSAGE":"NO_STOCK"}, status=400)
+        
+        cart.quantity = int(quantities)
+        cart.save()
+        
+        return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+
+    @login
+    def delete(self, request):
+        is_all = request.GET.get("is_all", None)
+        cart_id = request.GET.get("cart_id", None)
+
+        if not is_all:
+            Cart.objects.get(id = cart_id).delete()
+            return JsonResponse({"MESSAGE" : "DELETE"}, status=200)
+
+        Cart.objects.filter(member_id = request.user.id).delete()
+        
+        return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+
+
