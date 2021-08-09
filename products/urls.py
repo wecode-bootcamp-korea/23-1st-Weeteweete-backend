02a3efc468e256fb35f48ac363b1cbc1057e1662
임종성 <qkqndudnxld@gmail.com>
2021-08-09 10:57:49 +0900

    Detail View

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
-from django.urls  import path
-from products.views  import PageView
+from django.urls     import path
+from products.views  import PageView, DetailPageView
 
 urlpatterns = [
     path('', PageView.as_view()),
+    path('/<int:item_id>', DetailPageView.as_view()),
 ] 
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 import json
 
-from django.http     import JsonResponse
-from django.views    import View
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
 
-from products.models import Option, Product, Category, Concept, Item
+from products.models  import Option, Product, Category, Concept, Item
 
 class PageView(View):
      def get(self, request):
@@ -11,31 +12,18 @@ class PageView(View):
             product_id  = request.GET.get('product_id', None)
             category_id = request.GET.get('category_id', None)
             option_id   = request.GET.get('option_id', None)
+            main        = request.GET.get('main', None)
 
-            if not product_id and not category_id and not option_id:
-                items = Item.objects.order_by('-order_quantity')[:3]
-                main= [
-                    {
-                        'id'             : item.id,
-                        'name'           : item.name,
-                        'price'          : item.price,
-                        'discount'       : item.price - item.discount,
-                        'stock'          : item.stock,
-                        'image'          : item.image_set.get(main=1).image_url
-                    } for item in items]
-
-                return JsonResponse({'RESULTS': main if True else 'EMPTY'}, status=200)
-
-            if not Product.objects.filter(id=product_id).exists() or not Category.objects.filter(id=category_id).exists() or not Option.objects.filter(id=option_id):
+            if not main and (not Product.objects.filter(id=product_id).exists() or not Category.objects.filter(id=category_id).exists() or not Option.objects.filter(id=option_id)):
                 return JsonResponse({'MESSAGE':'NO_MENU'}, status=400)
 
-            concepts = Concept.objects.filter(category_id = category_id)
-
-            menu = [
+            concepts = Concept.objects.filter(item__category_id = category_id) if not main else Item.objects.order_by('-order_quantity')[:8] 
+            
+            results = [
                 {
-                        'concept'             : concept.name,
-                        'concept_description' : concept.content,
-                        'concpet_id'          : concept.id,
+                        'concept'             : concept.name if not main else None,
+                        'concept_description' : concept.content if not main else None,
+                        'concept_id'          : concept.id if not main else None,
                         'information'         : [
                             {
                                 'id'       : item.id,
@@ -43,11 +31,30 @@ class PageView(View):
                                 'price'    : item.price,
                                 'discount' : item.discount,
                                 'stock'    : item.stock,
-                                'color'    : item.color.name,
+                                'color'    : item.color.name if not item.color is None else None,
                                 'image'    : item.image_set.get(main=1).image_url
-                            } for item in Item.objects.filter(concept_id=concept.id, option_id=option_id)]
+                            } for item in (Item.objects.filter(Q(concept_id=concept.id)&Q(option_id=option_id)) if not main else [concept])]
                 } for concept in concepts]
-                
-            return JsonResponse({'RESULTS': menu if True else 'EMPTY'}, status=200)
+
+            return JsonResponse({'RESULTS': results}, status=200)
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+class DetailPageView(View):
+    def get(self, request, item_id):
+        if not Item.objects.filter(id=item_id).exists():
+            return JsonResponse({"MESSAGE":"NO_MENU"}, status=400)
+        
+        result = []
+        
+        item   = Item.objects.get(id=item_id)
+        
+        result.append({
+            "name"           : item.name,
+            "price"          : item.price,
+            "discount"       : item.discount,
+            "discount_price" : item.price-item.discount,
+            "image"          : [image.image_url for image in item.image_set.all()]
+        })
+
+        return JsonResponse({'RESULT':result}, status=200)
